476516e47628daaefd7f6b71bb2455a77b97797a
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-02 10:44:14 +0100

    Add audioRef currentTime

--- src/components/Player.js
+++ src/components/Player.js
@@ -32,6 +32,7 @@ const Player =({currentSong, isPlaying,setIsPlaying})=>{
         }
 
         const dragHandler =(e)=>{
+            audioRef.current.currentTime = e.target.value
             setSongInfo({...songInfo,currentTime: e.target.value})
 
         }
