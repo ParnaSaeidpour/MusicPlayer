74aeb3b0bc28ab96f8a56f62d07a1bcbf4b0c186
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-03 13:43:16 +0100

    Create Library component

---
+++ src/components/Library.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Library =()=>{
+    return(
+        <div className="Library">
+            <h2>Library</h2>
+            <div className="Librart-songs">
+            </div>
+            
+        </div>
+    )
+}
