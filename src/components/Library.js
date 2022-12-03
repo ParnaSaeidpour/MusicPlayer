b3333bf738465e8ae3bfed45f659beadc021256f
Parna Saeidpour <parnasaeidpour@gmail.com>
2022-12-03 13:54:29 +0100

    Import LibrarySong

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,10 +1,12 @@
 import React from "react";
+import LibrarySong from "./LibrarySong";
 
 const Library =()=>{
     return(
         <div className="Library">
             <h2>Library</h2>
             <div className="Librart-songs">
+                <LibrarySong />
             </div>
             
         </div>
